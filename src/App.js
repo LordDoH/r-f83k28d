917ccd00a540c67364c656682e9b3dcc419f0c19
Daniel <dochoahenao@gmail.com>
2021-12-21 23:46:34 -0500

    Extraida funcion onClick

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import './App.css';
 
 function App () {
   const [count,setCounter] = useState(0);
+
+  const onClick = () => {setCounter(count+1)}
+
     return (
       <div>
         <span className="value">{count}</span>
-        <button id="inc" onClick = {() => setCounter(count+1) } >Incrementa</button>
+        <button id="inc" onClick = {onClick} >Incrementa</button>
       </div>
     );
 }
